8002e07dd01b6fae723188a18f6d80b330f5fb48
kkishan083 <kishan.kumar@network.lilly.com>
2025-07-22 16:50:47 +0530

    Initial Code setup completed

---
+++ core/src/main/java/com/assets/core/models/TestComponent.java
@@ -0,0 +1,18 @@
+package com.assets.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class TestComponent {
+	@ValueMapValue
+    private String text;
+	
+	
+	String getText()
+	{
+		return text;
+	}
+}
